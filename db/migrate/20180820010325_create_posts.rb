870d0fef9b0eb477e0521b69379270660acd372c
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 21:08:55 -0400

    add tables

--- db/migrate/20180820010325_create_posts.rb
+++ db/migrate/20180820010325_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.string :caption
+      t.string :img_url
+      t.integer :likes
       t.timestamps
     end
   end
--- db/migrate/20180820010406_create_comments.rb
+++ db/migrate/20180820010406_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-
+      t.integer :post_id
+      t.integer :text
+      t.string :author
       t.timestamps
     end
   end
